792e184737b13e01eef78ec5301fb6070855806f
tsuguri <tsuguri4429@gmail.com>
2023-12-05 16:18:43 +0900

    first commit

---
+++ github-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github-app
+//
+//  Created by Ryo Yoshitsugu on R 5/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github-app/github_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_appApp.swift
+//  github-app
+//
+//  Created by Ryo Yoshitsugu on R 5/12/05.
+//
+
+import SwiftUI
+
+@main
+struct github_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
