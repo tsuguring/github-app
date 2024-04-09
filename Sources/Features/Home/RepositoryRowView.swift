0c5efc0915db64b8fa486250bc2c6ce111f1291b
tsuguri <tsuguri4429@gmail.com>
2024-04-09 22:12:01 +0900

    layout RepositoryRowView.swift

--- Sources/Features/Home/RepositoryRowView.swift
+++ Sources/Features/Home/RepositoryRowView.swift
@@ -11,10 +11,17 @@ import GitHubData
 struct RepositoryRowView: View {
     let sampleRepositoryData = Repository.sampleData
     var body: some View {
-        VStack {
-            ownerImage
-            Text(sampleRepositoryData.fullName)
-            Text(sampleRepositoryData.description ?? "")
+        VStack(alignment: .leading) {
+            HStack {
+                ownerImage
+                Text(sampleRepositoryData.owner.name)
+            }
+            title
+            description
+            HStack {
+                starCount
+                language
+            }
         }
     }
 }
@@ -30,7 +37,35 @@ private extension RepositoryRowView {
         } placeholder: {
             ProgressView()
         }
-        .frame(width: 30, height: 30)
+        .frame(width: 25, height: 25)
     }
     
+    var title: some View {
+        Text(sampleRepositoryData.fullName)
+            .bold()
+            .font(.system(size: 20))
+            .padding(.vertical, 1)
+    }
+    
+    @ViewBuilder var description: some View {
+        if let description = sampleRepositoryData.description {
+            Text(description).padding(.bottom, 1)
+        }
+    }
+    
+    var starCount: some View {
+        HStack(spacing: 0) {
+            Image(systemName: "star")
+            Text(String(sampleRepositoryData.starsCount))
+        }
+    }
+    
+    @ViewBuilder var language: some View {
+        if let language = sampleRepositoryData.language {
+            HStack(spacing: 0) {
+                Image(systemName: "text.word.spacing")
+                Text(language)
+            }.padding(.leading, 20)
+        }
+    }
 }
