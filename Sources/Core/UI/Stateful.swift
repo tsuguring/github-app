8e021cdbf3b9910e1222d9a737c52a404179c493
tsuguri <tsuguri4429@gmail.com>
2024-05-07 22:11:34 +0900

    modified Stateful

--- Sources/Core/UI/Stateful.swift
+++ Sources/Core/UI/Stateful.swift
@@ -1,8 +1,15 @@
 //
-//  State.swift
-//  
+//  Stateful.swift
+//
 //
 //  Created by Ryo Yoshitsugu on R 6/04/28.
 //
 
 import Foundation
+
+package enum Stateful<Value> {
+    case idle
+    case loading
+    case success(Value)
+    case failed(Error)
+}
