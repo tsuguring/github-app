b82841e60a19e58d872d8717c59f798a55badece
tsuguri <tsuguri4429@gmail.com>
2024-05-10 22:15:38 +0900

    error処理の追加

--- Sources/Core/Network/APIClient.swift
+++ Sources/Core/Network/APIClient.swift
@@ -14,14 +14,21 @@ import Foundation
     }()
     
     package func request<Request: BaseRequestProtocol>(with request: Request) async throws -> Request.ResponseType {
-        let result = try await session.data(for: request.buildURLRequest())
+        let result: (Data, URLResponse)
+        
+        do {
+            result = try await session.data(for: request.buildURLRequest())
+        } catch {
+            throw APIClientError.connectionError
+        }
+        
         try validate(data: result.0, response: result.1)
         return try JSONDecoder().decode(Request.ResponseType.self, from: result.0)
     }
     
     private func validate(data: Data, response: URLResponse) throws {
         guard let code = (response as? HTTPURLResponse)?.statusCode else {
-            throw APIClientError.connectionError(data)
+            throw APIClientError.connectionError
         }
         
         guard (200..<300).contains(code) else {
